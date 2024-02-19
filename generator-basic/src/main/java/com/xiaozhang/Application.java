e52f3790dd9f4e89cafc292c5945e96a6d373ba1
zhangsan <1764839951@qq.com>
2024-02-20 02:35:23 +0800

    项目demo测试

---
+++ generator-basic/src/main/java/com/xiaozhang/Application.java
@@ -0,0 +1,11 @@
+package main.java.com.xiaozhang;
+
+/**
+ * @author acal1314
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("project start");
+    }
+}
